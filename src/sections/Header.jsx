df016d9b824266820a53bc88657a0ee3f62fc5aa
Opata Joshua <opatajoshua@gmail.com>
2022-07-07 16:27:31 +0200

    experience and location update

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
                         <p className="mt-4">I am a passionate software developer with 6+ years of experience</p>
                         <span className="group">
                             <div className="flex items-center text-gray-400 mt-2">
-                            <FaMapMarkerAlt className="mr-1"/> Accra - Ghana | Remote
+                            <FaMapMarkerAlt className="mr-1"/> Frankfurt - Germany | Remote
                             </div>
             
                         </span>
@@ -26,7 +26,7 @@ export default function Header() {
                             {/* <div className="flex-1">ddd</div> */}
                         </div>
                         <div className="flex flex-row items-center mt-8">
-                            <p className="text-4xl mr-3">5+</p>
+                            <p className="text-4xl mr-3">6+</p>
                             <div className="text-xs text-gray-400">
                                 <p>YEARS</p>
                                 <p>EXPERIENCE</p>
